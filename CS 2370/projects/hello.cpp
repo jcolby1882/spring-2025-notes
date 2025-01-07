2dbb3d957db0e82ed6ae40fc8043f7916c2923a2
Jacob Colby <trex072004@gmail.com>
2025-01-07 09:40:53 -0700

    added first project for CS 2370

---
+++ CS 2370/projects/hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * Hello.cpp
+ * Jacob Colby
+ * outputs hello world and an int input from user
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int xxx;                             // defines an integer variable
+  cout << "Hello" << " World" << endl; // outputs line to comnsole
+  cin >> xxx;                          // takes input from console
+  cout << "xxx is:" << xxx << endl;    // outputs the input from line 8
+  return 0;
+}
